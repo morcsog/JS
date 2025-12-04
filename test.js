ac8810c5ae49b8ada09404993804f997d7276926
morcsog <morcsog99@gmail.com>
2025-12-04 20:52:26 +0100

    Add variables, objects, maps and loops

--- test.js
+++ test.js
@@ -10,14 +10,48 @@ let nullVar = null;
 let obj = {
     name: null,
     age: null
-}
+};
 
-console.log(obj.name)
+console.log(obj.name);
 obj.name = "Géza"
 console.log(obj.name);
+
+let arrVar = [1,2,3];
 
 let btn = document.getElementById('button');
 let text = document.getElementById('text');
 btn.addEventListener("click", (event) => {
     text.style.color = "red";
 });
+
+let mapVar = new Map();
+let user1 = {name: "Eszter"};
+let user2 = {name: "Gábor"};
+mapVar.set("name", "Géza");
+mapVar.set("age", 15);
+mapVar.set("hairColor", "Blue");
+mapVar.set(user1, "Admin");
+mapVar.set(user2, "Guest");
+
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
+
+for (let index = 0; index < array.length; index++) {
+    //anything, mostly arrays
+
+}
+
+for (const key in object) {
+ //objects       
+    
+}
+
+for (const [key, value] of mapVar) {
+    console.log(key, value);
+    
+}
